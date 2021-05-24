68fd2790802d8b53a201c4077adf1ff5e83dfc4e
maddiprudhviraj <maddiprudhviraj@gmail.com>
2021-05-24 11:29:35 +0530

    1 file modified, 3 deleted

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter Reportss</h1>`;
+appDiv.innerHTML = `<h1>JS Starter Reportsss</h1>`;
